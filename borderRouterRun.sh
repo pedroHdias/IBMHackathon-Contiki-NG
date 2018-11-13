347e8d04fead4f174660e6423a62384a2774b6f4
Pedro Henriques Dias <diaspedro1999@hotmail.com>
2018-11-13 16:47:17 +0000

    commit changes

--- borderRouterRun.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-COMMON="/home/pi/Desktop/VitaBox"
-OUTLOGFILE="$COMMON/Scripts/stdoutBorder.log"
-ERRLOGFILE="$COMMON/Scripts/stderrBorder.log"
-rm "$OUTLOGFILE"
-rm "$ERRLOGFILE"
-cd "$COMMON/contiki-ng/examples/rpl-border-router/"
-make TARGET=zoul savetarget >> "$OUTLOGFILE" 2>> "$ERRLOGFILE"
-make connect-router >> "$OUTLOGFILE" 2>> "$ERRLOGFILE"
